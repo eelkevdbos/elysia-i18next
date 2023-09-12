6d0d2c9227601e5336f2f08105ddd0498d5150b7
Eelke van den Bos <eelkevdbos@gmail.com>
2023-09-11 23:07:35 -0400

    Add exports to types and helper functions

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import Elysia, { PreContext } from 'elysia'
+import { Elysia, PreContext } from 'elysia'
 import lib, { InitOptions } from 'i18next'
 
-type I18NextPluginOptions = {
+export const t = lib.t
+
+export type I18NextPluginOptions = {
   initOptions: InitOptions
   detectLanguage: <T extends PreContext>(
     ctx: T
   ) => null | string | Promise<string | null>
 }
 
-const detectLanguage = (ctx: PreContext) => {
+export const detectLanguage = (ctx: PreContext) => {
   if ('language' in ctx.store) {
     return ctx.store.language as string | null
   }
@@ -21,17 +23,19 @@ const defaultOptions: I18NextPluginOptions = {
 }
 
 export const i18next = (userOptions: Partial<I18NextPluginOptions>) => {
-  const options: I18NextPluginOptions = {
-    ...defaultOptions,
-    ...userOptions,
-  }
-  const i18next = new Elysia({ name: 'elysia-i18next' }).derive(async ctx => {
-    await lib.init(options.initOptions || {})
-    const lng = await options.detectLanguage<typeof ctx>(ctx)
-    if (lng) {
-      await lib.changeLanguage(lng)
-    }
-    return { i18next: lib, t: lib.t }
-  })
-  return (app: Elysia) => app.use(i18next)
+  return (app: Elysia) =>
+    app.use(
+      new Elysia({ name: 'elysia-i18next' }).derive(async ctx => {
+        const options: I18NextPluginOptions = {
+          ...defaultOptions,
+          ...userOptions,
+        }
+        await lib.init(options.initOptions || {})
+        const lng = await options.detectLanguage<typeof ctx>(ctx)
+        if (lng) {
+          await lib.changeLanguage(lng)
+        }
+        return { i18next: lib, t: lib.t }
+      })
+    )
 }
